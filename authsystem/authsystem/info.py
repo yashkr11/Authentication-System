eef66f93d4c72ce4f21c981047597921328e76b3
yashkr11 <106267816+yashkr11@users.noreply.github.com>
2022-10-11 12:01:29 +0530

    Add files via upload

--- authsystem/authsystem/info.py
+++ authsystem/authsystem/info.py
@@ -1,5 +1,6 @@
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gfg.demo.django.login'
-EMAIL_HOST_PASSWORD = 'gfgdemo123'
+EMAIL_HOST_USER = 'dummyid4ecell@gmail.com'
+EMAIL_HOST_PASSWORD = 'eparaodhfohgrthy'
 EMAIL_PORT = 587
--- authsystem/users/views.py
+++ authsystem/users/views.py
@@ -1,3 +1,4 @@
+from tokenize import generate_tokens
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -9,49 +10,96 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth import authenticate, login, logout
+from . tokens import generate_token
 
 
 def home(request):
  return render (request, "users/index.html")
 
 def signup(request):
- if request.method == "POST":
-     username=request.POST.get('username')
-     fname=request.POST.get('fname')
-     sname=request.POST.get('sname')
-     email=request.POST.get('email')
-     pass1=request.POST.get('pass1')
-     pass2=request.POST.get('pass2')
-     if User.objects.filter(username=username):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        sname = request.POST['sname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        if User.objects.filter(username=username):
             messages.error(request, "Username already exist! Please try some other username.")
             return redirect('home')
         
-     if User.objects.filter(email=email).exists():
+        if User.objects.filter(email=email).exists():
             messages.error(request, "Email Already Registered!!")
             return redirect('home')
         
-     if pass1 != pass2:
+        if len(username)>20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('home')
+        
+        if pass1 != pass2:
             messages.error(request, "Passwords didn't matched!!")
             return redirect('home')
         
-     if not username.isalnum():
+        if not username.isalnum():
             messages.error(request, "Username must be Alpha-Numeric!!")
             return redirect('home')
-     myuser= User.objects.create_user(username, email, pass1) 
-     myuser.first_name= fname
-     myuser.save()
-     
-     messages.success(request, "You have been successfully registered !!")
-
-     subject = "Welcome to Django Login!!"
-     message = "Hello " + myuser.first_name + "!! \n" + "Welcome !! We have also sent you a confirmation email, please confirm your email address."        
-     from_email = settings.EMAIL_HOST_USER
-     to_list = [myuser.email]
-     send_mail(subject, message, from_email, to_list, fail_silently=True)
         
-     return redirect ("signin")
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = sname
+        
+        myuser.is_active = False
+        myuser.save()
+        messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+        
+       
+        subject = "Welcome to our website Login!!"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome!! \nThank you for visiting our website.\n We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nYash Kumar"        
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+       
+        current_site = get_current_site(request)
+        email_subject = "Confirm your Email for Login!!"
+        message2 = render_to_string('email_conf.html',{
+            
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+        email_subject,
+        message2,
+        settings.EMAIL_HOST_USER,
+        [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+        
+        return redirect('signin')
+        
+        
+    return render(request, "users/signup.html")
 
- return render (request, "users/signup.html")
+def activate(request,uidb64,token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request,myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('signin')
+    else:
+        return render(request,'activation_failed.html')
 
 def signin(request):
  if request.method == "POST":
